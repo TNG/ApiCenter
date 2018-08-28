6756fe79ea40fd5a77131a717187260c61f78a35
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-08-28 13:56:53 +0200

    Add component for search
    In order to show a search function in the header to be able to search for specifications, a new component was added.
    
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

---
+++ frontend/src/app/specification-search/specification-search.component.spec.ts
@@ -0,0 +1,6 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpecificationSearchComponent } from './specification-search.component';
+
+describe('SpecificationSearchComponent', () => {
+});
