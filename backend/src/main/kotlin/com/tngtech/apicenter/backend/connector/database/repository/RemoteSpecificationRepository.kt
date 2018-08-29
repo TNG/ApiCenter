9526d7540a4a09dcce8cc4b5013a757f59bd5928
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-08-29 10:02:01 +0200

    Remove unused classes
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/repository/RemoteSpecificationRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.tngtech.apicenter.backend.connector.database.repository
-
-import com.tngtech.apicenter.backend.connector.database.entity.RemoteSpecificationEntity
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface RemoteSpecificationRepository : CrudRepository<RemoteSpecificationEntity, UUID>
