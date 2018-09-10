4f9b97edc23206d9102d66df8b77f6d3f925fb6a
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-09-10 11:41:18 +0200

    Add API descriptions.
    Add descriptions to models and endpoints to make the generated documentation better readable.
    
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/SpecificationController.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/SpecificationController.kt
@@ -5,6 +5,8 @@ import com.tngtech.apicenter.backend.connector.rest.dto.SpecificationFileDto
 import com.tngtech.apicenter.backend.connector.rest.mapper.SpecificationMapper
 import com.tngtech.apicenter.backend.connector.rest.service.SynchronizationService
 import com.tngtech.apicenter.backend.domain.handler.SpecificationHandler
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -20,6 +22,7 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/specifications")
+@Api(description = "Operations to create, update and maintain specifications")
 class SpecificationController @Autowired constructor(
     private val specificationHandler: SpecificationHandler,
     private val synchronizationService: SynchronizationService,
@@ -28,6 +31,7 @@ class SpecificationController @Autowired constructor(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Upload a new specification, or define a remote specification by its url")
     fun uploadSpecification(@RequestBody specificationFileDto: SpecificationFileDto): SpecificationDto {
         val specification = specificationMapper.toDomain(specificationFileDto)
 
@@ -37,6 +41,7 @@ class SpecificationController @Autowired constructor(
     }
 
     @PutMapping("/{specificationId}")
+    @ApiOperation("Update an existing specification")
     fun updateSpecification(@RequestBody specificationFileDto: SpecificationFileDto, @PathVariable specificationId: String): SpecificationDto {
 
         val specification = specificationMapper.toDomain(
@@ -53,24 +58,29 @@ class SpecificationController @Autowired constructor(
     }
 
     @GetMapping
+    @ApiOperation("List all existing specifications")
     fun findAllSpecifications(): List<SpecificationDto> =
         specificationHandler.findAll().map { spec -> specificationMapper.fromDomain(spec) }
 
     @GetMapping("/{specificationId}")
+    @ApiOperation("Find a specific specification by id")
     fun findSpecification(@PathVariable specificationId: UUID): SpecificationDto =
         specificationMapper.fromDomain(specificationHandler.findOne(specificationId)!!)
 
     @DeleteMapping("/{specificationId}")
+    @ApiOperation("Delete a specification by id")
     fun deleteSpecification(@PathVariable specificationId: UUID) {
         specificationHandler.delete(specificationId)
     }
 
     @PostMapping("/{specificationId}/synchronize")
+    @ApiOperation("Synchronize an existing specification with its remote source")
     fun synchronizeSpecification(@PathVariable specificationId: UUID) {
         synchronizationService.synchronize(specificationId)
     }
 
     @GetMapping("/search/{searchString}")
+    @ApiOperation("Search for specifications by a search string")
     fun searchSpecification(@PathVariable searchString: String): List<SpecificationDto> =
         specificationHandler.search(searchString).map { spec -> specificationMapper.fromDomain(spec) }
 }
--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/dto/SpecificationFileDto.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/dto/SpecificationFileDto.kt
@@ -1,5 +1,13 @@
 package com.tngtech.apicenter.backend.connector.rest.dto
+
 import com.fasterxml.jackson.annotation.JsonCreator
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import java.util.UUID
 
-data class SpecificationFileDto @JsonCreator constructor(val fileContent: String?, val fileUrl: String? = "", val id: UUID? = null)
+@ApiModel(description = "Represents an OpenAPI-File")
+data class SpecificationFileDto @JsonCreator constructor(
+    @ApiModelProperty("The content of a file to be uploaded - either this or fileUrl has to be set.") val fileContent: String?,
+    @ApiModelProperty("The url of a remote file to be loaded - either this or fileContent has to be set.") val fileUrl: String? = "",
+    @ApiModelProperty("The uuid of an already existing specification (optional)") val id: UUID? = null
+)
