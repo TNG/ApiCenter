856f3b43cea8f1ff0ac808ac316130b16ea2a08f
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-09-19 11:09:34 +0200

    Remove Springfox and OpenAPI-Documentation-Annotations on controllers and DTOs
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/SpecificationController.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/SpecificationController.kt
@@ -5,8 +5,6 @@ import com.tngtech.apicenter.backend.connector.rest.dto.SpecificationFileDto
 import com.tngtech.apicenter.backend.connector.rest.mapper.SpecificationMapper
 import com.tngtech.apicenter.backend.connector.rest.service.SynchronizationService
 import com.tngtech.apicenter.backend.domain.handler.SpecificationHandler
-import io.swagger.annotations.Api
-import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -22,7 +20,6 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/specifications")
-@Api(description = "Operations to create, update and maintain specifications")
 class SpecificationController @Autowired constructor(
     private val specificationHandler: SpecificationHandler,
     private val synchronizationService: SynchronizationService,
@@ -31,7 +28,6 @@ class SpecificationController @Autowired constructor(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("Upload a new specification, or define a remote specification by its url")
     fun uploadSpecification(@RequestBody specificationFileDto: SpecificationFileDto): SpecificationDto {
         val specification = specificationMapper.toDomain(specificationFileDto)
 
@@ -41,7 +37,6 @@ class SpecificationController @Autowired constructor(
     }
 
     @PutMapping("/{specificationId}")
-    @ApiOperation("Update an existing specification")
     fun updateSpecification(@RequestBody specificationFileDto: SpecificationFileDto, @PathVariable specificationId: String): SpecificationDto {
 
         val specification = specificationMapper.toDomain(
@@ -58,29 +53,24 @@ class SpecificationController @Autowired constructor(
     }
 
     @GetMapping
-    @ApiOperation("List all existing specifications")
     fun findAllSpecifications(): List<SpecificationDto> =
         specificationHandler.findAll().map { spec -> specificationMapper.fromDomain(spec) }
 
     @GetMapping("/{specificationId}")
-    @ApiOperation("Find a specific specification by id")
     fun findSpecification(@PathVariable specificationId: UUID): SpecificationDto =
         specificationMapper.fromDomain(specificationHandler.findOne(specificationId)!!)
 
     @DeleteMapping("/{specificationId}")
-    @ApiOperation("Delete a specification by id")
     fun deleteSpecification(@PathVariable specificationId: UUID) {
         specificationHandler.delete(specificationId)
     }
 
     @PostMapping("/{specificationId}/synchronize")
-    @ApiOperation("Synchronize an existing specification with its remote source")
     fun synchronizeSpecification(@PathVariable specificationId: UUID) {
         synchronizationService.synchronize(specificationId)
     }
 
     @GetMapping("/search/{searchString}")
-    @ApiOperation("Search for specifications by a search string")
     fun searchSpecification(@PathVariable searchString: String): List<SpecificationDto> =
         specificationHandler.search(searchString).map { spec -> specificationMapper.fromDomain(spec) }
 }
