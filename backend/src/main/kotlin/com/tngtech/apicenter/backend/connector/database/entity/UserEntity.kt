1f0a14ef11595eac8bfa2dda2f2402c74afab2bc
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-09-26 15:46:09 +0200

    Introduce user to domain
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

---
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/entity/UserEntity.kt
@@ -0,0 +1,14 @@
+package com.tngtech.apicenter.backend.connector.database.entity
+
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class UserEntity(
+    @Id @GeneratedValue val id: UUID,
+    val username: String,
+    val email: String,
+    val origin: String,
+    val externalId: String)
