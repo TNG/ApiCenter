42e2a52436af84b3c0f785adafc7fd71446302d1
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-10-08 13:16:10 +0200

    Restructure frontend to support specifications with multiple versions.
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- frontend/src/app/specification-search-detail/specification-search-detail.component.spec.ts
+++ frontend/src/app/specification-search-detail/specification-search-detail.component.spec.ts
@@ -1,9 +1,10 @@
-import { SpecificationSearchDetailComponent } from './specification-search-detail.component';
+import {SpecificationSearchDetailComponent} from './specification-search-detail.component';
 import {SpecificationService} from '../specification.service';
 import {instance, mock, verify, when} from 'ts-mockito';
 import {ActivatedRoute} from '@angular/router';
 import {Specification} from '../models/specification';
 import {from} from 'rxjs/index';
+import {Version} from '../models/version';
 
 describe('SpecificationSearchDetailComponent', () => {
   let specificationSearchDetailComponent: SpecificationSearchDetailComponent;
@@ -11,7 +12,8 @@ describe('SpecificationSearchDetailComponent', () => {
   const specificationService = instance(mockedSpecificationService);
   const mockedActivatedRoute = mock(ActivatedRoute);
   const activatedRoute = instance(mockedActivatedRoute);
-  const specifications = [new Specification('b0fb472d-bee2-47b6-8ecf-ee5e1e76e990', 'Test', 'Description', '1.0', 'Content', null)];
+  const versions = [new Version('0bb3274f-4d8b-405d-8148-4288bab65289', '1.0', 'Content')];
+  const specifications = [new Specification('b0fb472d-bee2-47b6-8ecf-ee5e1e76e990', 'Test', 'Description', versions, null)];
 
   beforeEach(() => {
     specificationSearchDetailComponent = new SpecificationSearchDetailComponent(specificationService, activatedRoute);
