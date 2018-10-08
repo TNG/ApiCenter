b6a9b80f0a4e7857e57f88d9931b695a51336263
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-10-08 15:26:51 +0200

    Add frontend functionality to delete specific versions.
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

---
+++ frontend/src/app/version.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Specification} from "./models/specification";
+import {Observable, throwError} from "rxjs";
+import {Version} from "./models/version";
+
+@Injectable()
+export class VersionService {
+
+  private versionUrl = environment.apiUrl + '/versions';
+
+  constructor(private http: HttpClient) { }
+
+  public getVersion(versionId: string) {
+    return this.http.get<Version>(this.versionUrl + '/' + versionId)
+      .catch((error: any) => throwError(error || 'Server error'));
+  }
+
+  public deleteVersion(versionId: string): Observable<Version> {
+    return this.http.delete<Version>(this.versionUrl + '/' + versionId)
+      .catch((error: any) => throwError(error || 'Server error'));
+  }
+}
