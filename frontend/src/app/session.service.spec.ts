41ea5978fb301e394689e2c854952374b61fc8ed
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2018-10-10 16:00:04 +0200

    Remove unnecessary test.
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- frontend/src/app/session.service.spec.ts
+++ frontend/src/app/session.service.spec.ts
@@ -1,26 +1,4 @@
 import {SessionService} from './session.service';
-import {instance, mock, when} from 'ts-mockito';
-import {HttpClient} from '@angular/common/http';
-import {SessionToken} from './models/sessiontoken';
 
 describe('SessionService', () => {
-
-  let sessionService: SessionService;
-  const mockedHttpClient = mock(HttpClient);
-  const httpClient = instance(mockedHttpClient);
-  const token = new SessionToken('Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9', 'username');
-  const loginReq = { username: 'username', password: 'password' };
-
-  beforeEach(() => {
-    sessionService = new SessionService(httpClient);
-  });
-
-  /*it('should return authentication token', () => {
-    when(mockedHttpClient.post('http://localhost:8080/sessions', loginReq)).thenReturn(from([token]));
-
-    sessionService.login('username', 'password').subscribe((data: SessionToken) => {
-      expect(data).toBe(token);
-    });
-  });*/
-
 });
