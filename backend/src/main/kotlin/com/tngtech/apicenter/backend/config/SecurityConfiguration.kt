af93f66e2c48ed8b607aa8965e384a2be3e42ae6
Philippa Hack <philippa.hack@tngtech.com>
2019-01-21 14:46:45 +0100

    Restrict permitted resource requests
    In the last commit, any and all resource requests were permitted. This was
    simply an attempt to make the static (Angular-generated) resources fetchable
    in any way possible, so that the site could be used. Of course, the downside
    is that removes the authentification provided by the JWT system entirely.
    
    After researching, it seems the only solution online within Spring is to
    list every resource individually.
    
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/config/SecurityConfiguration.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/config/SecurityConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -25,12 +26,33 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
             .csrf().disable()
             .authorizeRequests()
             .antMatchers("/sessions").permitAll()
-            .and()
-            .authorizeRequests()
+
+            .antMatchers("/").permitAll()
+            .antMatchers("/3rdpartylicenses.txt").permitAll()
+            .antMatchers("/favicon.ico").permitAll()
+            .antMatchers("/index.html").permitAll()
+            .antMatchers("/main.js").permitAll()
+                .antMatchers("/main.js.map").permitAll()
+            .antMatchers("/open-iconic.*").permitAll()
+            .antMatchers("/polyfills.js").permitAll()
+                .antMatchers("/polyfills.js.map").permitAll()
+            .antMatchers("/runtime.js").permitAll()
+                .antMatchers("/runtime.js.map").permitAll()
+            .antMatchers("/styles.js").permitAll()
+                .antMatchers("/styles.js.map").permitAll()
+            .antMatchers("/scripts.js").permitAll()
+                .antMatchers("/scripts.js.map").permitAll()
+            .antMatchers("/vendor.js").permitAll()
+                .antMatchers("/vendor.js.map").permitAll()
             .anyRequest().authenticated()
             .and()
             .addFilter(JwtAuthorizationFilter(authenticationManager(), jwtSecuritySecret))
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers("/resources/**")
+    }
 }
+
