51c3f9cb7fe1dc907bc70e0954d74de8522529fd
Philippa Hack <philippa.hack@tngtech.com>
2019-01-23 13:17:59 +0100

    Remove unused imports
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/config/ContentNegotiationConfiguration.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/config/ContentNegotiationConfiguration.kt
@@ -1,9 +1,5 @@
 package com.tngtech.apicenter.backend.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
