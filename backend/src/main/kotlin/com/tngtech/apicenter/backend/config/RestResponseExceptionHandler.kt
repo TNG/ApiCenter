d63b4309a94f9bef9f909eaddadd9388407f6259
Philippa Hack <philippa.hack@tngtech.com>
2019-02-28 08:53:31 +0100

    Add HTTP reason phrase and refactor
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/config/RestResponseExceptionHandler.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/config/RestResponseExceptionHandler.kt
@@ -7,47 +7,43 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 import mu.KotlinLogging
-import java.util.*
+import java.util.UUID
 
 private val logger = KotlinLogging.logger {  }
 class SpecificationNotFoundException(val specificationId: UUID, val version: String? = "") : RuntimeException()
-class SpecificationParseException(val msg: String) : RuntimeException()
+class SpecificationParseException(val userMessage: String) : RuntimeException()
 class VersionAlreadyExistsException(val specificationTitle: String) : RuntimeException()
 
 data class ErrorMessage(
-        val message: String,
+        val httpReasonPhrase: String,
+        val userMessage: String,
         val timestamp: String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
 )
+
+fun responseBuilder(userMessage: String, status: HttpStatus): ResponseEntity<ErrorMessage> {
+    return ResponseEntity(
+        ErrorMessage(status.reasonPhrase, userMessage),
+        null,
+        status
+    )
+}
 
 @ControllerAdvice
 class RestResponseExceptionHandler {
 
     @ExceptionHandler(SpecificationNotFoundException::class)
     fun handleNotFound(exc: SpecificationNotFoundException): ResponseEntity<ErrorMessage> {
-        logger.info("Specification ${exc.specificationId} ${exc.version} not found")
-        return ResponseEntity(
-                ErrorMessage("Specification not found"),
-                null,
-                HttpStatus.NOT_FOUND
-        )
+        val detailedMessage = "Specification ${exc.specificationId} ${exc.version} not found"
+        logger.info(detailedMessage, exc)
+        return responseBuilder("Specification not found", HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(SpecificationParseException::class)
-    fun handleBadRequest(exc: SpecificationParseException): ResponseEntity<ErrorMessage> {
-        return ResponseEntity(
-                ErrorMessage(exc.msg),
-                null,
-                HttpStatus.BAD_REQUEST
-        )
-    }
+    fun handleBadRequest(exc: SpecificationParseException) =
+            responseBuilder(exc.userMessage, HttpStatus.BAD_REQUEST)
 
     @ExceptionHandler(VersionAlreadyExistsException::class)
-    fun handleVersionAlreadyExists(exc: VersionAlreadyExistsException): ResponseEntity<ErrorMessage> {
-        return ResponseEntity(
-                ErrorMessage("A specification with the same version already exists for ${exc.specificationTitle}"),
-                null,
-                HttpStatus.CONFLICT
-        )
-    }
+    fun handleVersionAlreadyExists(exc: VersionAlreadyExistsException) =
+            responseBuilder("A specification with the same version already exists for ${exc.specificationTitle}", HttpStatus.CONFLICT)
 }
 
