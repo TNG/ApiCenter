4fe350702d9a9a77712fe9eba7535eca9ed25ba1
Philippa Hack <philippa.hack@tngtech.com>
2019-03-18 16:45:27 +0100

    Add untested chmod REST handler
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/VersionController.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/rest/controller/VersionController.kt
@@ -2,21 +2,30 @@ package com.tngtech.apicenter.backend.connector.rest.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import com.tngtech.apicenter.backend.domain.exceptions.SpecificationNotFoundException
+import com.tngtech.apicenter.backend.domain.entity.Version
 import com.tngtech.apicenter.backend.connector.rest.dto.VersionDto
 import com.tngtech.apicenter.backend.connector.rest.mapper.VersionDtoMapper
 import com.tngtech.apicenter.backend.domain.service.VersionPersistenceService
+import com.tngtech.apicenter.backend.connector.acl.service.SpecificationPermissionManager
+import com.tngtech.apicenter.backend.connector.rest.security.JwtAuthenticationProvider
 import org.springframework.web.bind.annotation.*
 import org.springframework.http.MediaType
 import com.tngtech.apicenter.backend.domain.entity.ServiceId
-import com.tngtech.apicenter.backend.domain.entity.Version
-import com.tngtech.apicenter.backend.domain.exceptions.SpecificationNotFoundException
 import mu.KotlinLogging
+import org.springframework.security.acls.domain.BasePermission
+import org.springframework.security.acls.domain.PrincipalSid
+import org.springframework.security.core.context.SecurityContextHolder
+import java.lang.Long.parseLong
 
 private val logger = KotlinLogging.logger {}
 private const val MEDIA_TYPE_YAML = "application/yml"
 
 @RestController
-class VersionController constructor(private val versionPersistenceService: VersionPersistenceService, private val versionDtoMapper: VersionDtoMapper) {
+class VersionController constructor(private val versionPersistenceService: VersionPersistenceService,
+                                    private val versionDtoMapper: VersionDtoMapper,
+                                    private val permissionManager: SpecificationPermissionManager,
+                                    private val jwtAuthenticationProvider: JwtAuthenticationProvider) {
 
     @RequestMapping("/api/v1/specifications/{specificationId}/versions/{version}",
             produces = [MediaType.APPLICATION_JSON_VALUE,
@@ -47,5 +56,35 @@ class VersionController constructor(private val versionPersistenceService: Versi
     @DeleteMapping("/api/v1/specifications/{specificationId}/versions/{version}")
     fun deleteVersion(@PathVariable specificationId: String, @PathVariable version: String) {
         versionPersistenceService.delete(ServiceId(specificationId), version)
+    }
+
+    @PostMapping("/api/v1/specifications/{specificationId}/versions/{version}/chmod/{principal}")
+    fun chmodVersion(@PathVariable specificationId: String,
+                     @PathVariable version: String,
+                     @PathVariable principal: String,
+                     @RequestHeader(value = "Authorization") authHeader: String,
+                     @RequestParam(value = "read", required = false) grantRead: Boolean,
+                     @RequestParam(value = "write", required = false) grantWrite: Boolean
+    ) {
+        // TODO: Return 400 when principal doesn't exist (should be one of user, group, or 'all', 'everyone' keyword)
+
+        val username = jwtAuthenticationProvider.extractUsername(authHeader)
+        val sid = PrincipalSid(username)
+        logger.info(SecurityContextHolder.getContext().authentication.toString())
+        val asLong = try { parseLong(specificationId) } catch (exc: NumberFormatException) { throw GiveUpOnAclException() }
+
+        // exceptions may be thrown when attempting modifications, which are not yet handled
+        if (grantRead) {
+            permissionManager.addPermission(asLong, sid, BasePermission.READ)
+        } else {
+            permissionManager.removePermission(asLong, sid, BasePermission.READ)
+        }
+
+        if (grantWrite) {
+            permissionManager.addPermission(asLong, sid, BasePermission.WRITE)
+        } else {
+            permissionManager.removePermission(asLong,sid, BasePermission.WRITE)
+        }
+
     }
 }
