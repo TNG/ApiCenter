cf0bea987e1465d2939e3ae91d61711c63d63f5d
Philippa Hack <philippa.hack@tngtech.com>
2019-04-11 09:36:55 +0200

    Remove unecessary converters
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/mapper/configurer/SpecificationEntityMappingConfigurer.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/mapper/configurer/SpecificationEntityMappingConfigurer.kt
@@ -1,19 +1,15 @@
 package com.tngtech.apicenter.backend.connector.database.mapper.configurer
 
 import com.tngtech.apicenter.backend.connector.database.entity.SpecificationEntity
-import com.tngtech.apicenter.backend.connector.database.mapper.converter.SpecificationEntityConverter
 import com.tngtech.apicenter.backend.domain.entity.Specification
 import ma.glasnost.orika.MapperFactory
 import net.rakugakibox.spring.boot.orika.OrikaMapperFactoryConfigurer
 import org.springframework.stereotype.Component
 
 @Component
-class SpecificationEntityMappingConfigurer constructor(private val specificationEntityConverter: SpecificationEntityConverter) :
-        OrikaMapperFactoryConfigurer {
+class SpecificationEntityMappingConfigurer : OrikaMapperFactoryConfigurer {
 
     override fun configure(orikaMapperFactory: MapperFactory) {
-        orikaMapperFactory.converterFactory.registerConverter(specificationEntityConverter)
-
         orikaMapperFactory.classMap(Specification::class.java, SpecificationEntity::class.java)
             .field("id.id", "id")
             .byDefault()
--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/mapper/configurer/VersionEntityMappingConfigurer.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/database/mapper/configurer/VersionEntityMappingConfigurer.kt
@@ -1,8 +1,6 @@
 package com.tngtech.apicenter.backend.connector.database.mapper.configurer
 
-import com.tngtech.apicenter.backend.connector.database.entity.VersionEntity
 import com.tngtech.apicenter.backend.connector.database.mapper.converter.VersionEntityConverter
-import com.tngtech.apicenter.backend.domain.entity.Version
 import ma.glasnost.orika.MapperFactory
 import net.rakugakibox.spring.boot.orika.OrikaMapperFactoryConfigurer
 import org.springframework.stereotype.Component
@@ -13,14 +11,5 @@ class VersionEntityMappingConfigurer constructor(private val versionEntityConver
 
     override fun configure(orikaMapperFactory: MapperFactory) {
         orikaMapperFactory.converterFactory.registerConverter(versionEntityConverter)
-
-        orikaMapperFactory.classMap(VersionEntity::class.java, Version::class.java)
-            .field("versionId.version", "metadata.version")
-            .field("title", "metadata.title")
-            .field("description", "metadata.description")
-            .field("language", "metadata.language")
-            .field("endpointUrl", "metadata.endpointUrl")
-            .byDefault()
-            .register()
     }
 }
