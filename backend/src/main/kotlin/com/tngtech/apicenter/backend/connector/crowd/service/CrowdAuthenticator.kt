d12c287a3af1dbcda67a25e4fa01461dee51884e
Philippa Hack <philippa.hack@tngtech.com>
2019-07-17 10:02:29 +0200

    Refactor, pull out return statement
    Signed-off-by: Philippa Hack <philippa.hack@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/crowd/service/CrowdAuthenticator.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/backend/connector/crowd/service/CrowdAuthenticator.kt
@@ -12,12 +12,11 @@ import java.util.UUID
 @ConditionalOnProperty(name = ["auth.service"], havingValue = "crowd")
 class CrowdAuthenticator @Autowired constructor(private val crowdClient: CrowdClient) : ExternalAuthenticator {
 
-    override fun authenticate(username: String, password: String): User? {
+    override fun authenticate(username: String, password: String): User? =
         try {
             val user = crowdClient.authenticateUser(username, password)
-            return User(UUID.randomUUID(), user.name, user.emailAddress, "crowd", user.externalId)
+            User(UUID.randomUUID(), user.name, user.emailAddress, "crowd", user.externalId)
         } catch (exception: Exception) {
-            return null
+            null
         }
-    }
 }
