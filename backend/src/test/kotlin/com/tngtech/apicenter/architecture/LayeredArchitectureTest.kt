4fa3bd503eb31c9b5cffefef77b0a4bf4967eca8
Tobias Kunz <tobias.kunz@tngtech.com>
2020-01-27 19:01:19 +0100

    refact: Architecture tests for layered architecture
    Signed-off-by: Tobias Kunz <tobias.kunz@tngtech.com>

---
+++ backend/src/test/kotlin/com/tngtech/apicenter/architecture/LayeredArchitectureTest.kt
@@ -0,0 +1,42 @@
+package com.tngtech.apicenter.architecture
+
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+
+@AnalyzeClasses(packages = arrayOf("com.tngtech.apicenter"), importOptions = [ImportOption.DontIncludeTests::class] )
+class LayerArchitectureTest {
+
+    @ArchTest
+    val enforceDtoShouldNotDependOnRepositoryOrEntity: ArchRule? = noClasses().that().resideInAnyPackage("..repository..", "..entity..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAPackage("..dto..")
+
+    @ArchTest
+    val enforceRestControllerShouldNotAccessDatabaseLayer: ArchRule? = noClasses().that().resideInAnyPackage("..controller..", "..dto..")
+            .should()
+            .accessClassesThat()
+            .resideInAnyPackage("..entity..", "..repository..")
+
+    @ArchTest
+    val enforceServiceDoesNotAccessUpwards: ArchRule? = noClasses().that().resideInAnyPackage("..service..")
+            .should()
+            .accessClassesThat()
+            .resideInAnyPackage("..controller..")
+
+    @ArchTest
+    val enforceRepositoryDoesNotAccessUpwards: ArchRule? = noClasses().that().resideInAnyPackage("..repository..")
+            .should()
+            .accessClassesThat()
+            .resideInAnyPackage("..service..", "controller")
+
+    @ArchTest
+    val ensureUtilsAreIndependent: ArchRule = noClasses().that().resideInAPackage("..util..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..manager..", "..business..", "..client..")
+}
