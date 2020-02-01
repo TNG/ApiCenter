cfdf54f5ae255247afaf4f8522ed70425426b5ca
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-02-01 17:51:52 +0100

    feat: add validations to ApplicationEntity
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/entity/ApplicationEntity.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/entity/ApplicationEntity.kt
@@ -2,11 +2,12 @@ package com.tngtech.apicenter.entity
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.validation.constraints.NotBlank
 
 @Entity
 data class ApplicationEntity(
-    @Id val applicationId: String?,
-    val name: String,
-    val description: String,
-    val contact: String
+        @Id val applicationId: String?,
+        @NotBlank val name: String,
+        @NotBlank val description: String,
+        @NotBlank val contact: String
 )
