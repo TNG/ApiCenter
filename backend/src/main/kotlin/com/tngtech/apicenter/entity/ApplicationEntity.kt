8d00f4057228cf95e05164f9cecbc89f03bbac78
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-02-16 16:17:00 +0100

    store id as uuid in database
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/entity/ApplicationEntity.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/entity/ApplicationEntity.kt
@@ -1,12 +1,18 @@
 package com.tngtech.apicenter.entity
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
+import org.hibernate.annotations.GenericGenerator
 
 @Entity
 data class ApplicationEntity(
-    @Id val id: String?,
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String?,
+
     @NotBlank val name: String,
     @NotBlank val description: String,
     @NotBlank val contact: String
