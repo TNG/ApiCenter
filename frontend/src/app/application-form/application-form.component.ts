8b879f4573e33cfee37b22ea979ebd9a85265386
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-02-23 13:55:11 +0100

    Add application form component and load it in a dialog when add-button is clicked
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

---
+++ frontend/src/app/application-form/application-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-form',
+  templateUrl: './application-form.component.html',
+  styleUrls: ['./application-form.component.scss']
+})
+export class ApplicationFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
