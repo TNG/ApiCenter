3492c6bbf46aba9d14c73c0aefddb909ae39ddb6
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-03-08 14:04:31 +0100

    refactor header to not use table
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,4 @@ import {Component} from '@angular/core';
 })
 export class HeaderComponent {
   title = 'ApiCenter';
-  loggedInUser = 'Denise';
 }
