3dc9925ee349e63cf71a3407d84a32bf7005bf6a
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-03-14 16:47:00 +0100

    refactor global error handling to use side effects instead of error message field in state
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- frontend/src/app/store/selectors/error.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { AppState } from '../state/app.state';
-import { createSelector } from '@ngrx/store';
-
-const selectErrorState = (state: AppState) => state.errorState;
-
-export const selectErrorMessage = createSelector(
-  selectErrorState,
-  state => state.errorMessage
-);
--- frontend/src/app/store/state/app.state.ts
+++ frontend/src/app/store/state/app.state.ts
@@ -1,7 +1,5 @@
 import { ApplicationState } from '../../application/store/state/application.state';
-import { ErrorState } from './error.state';
 
 export interface AppState {
   applicationState: ApplicationState;
-  errorState: ErrorState;
 }
