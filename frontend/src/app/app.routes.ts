e48c694a184b5c73fc8165160725c16553f92e6f
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-04-05 09:23:00 +0200

    replace export of routes
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,7 @@ import { Routes } from '@angular/router';
 import { ApplicationOverviewComponent } from './application/application-overview/application-overview.component';
 import { ApplicationDetailsComponent } from './application/application-details/application-details.component';
 
-const appRoutes: Routes = [
+export const appRoutes: Routes = [
   { path: '', component: ApplicationOverviewComponent },
   { path: 'applications/:id', component: ApplicationDetailsComponent }
 ];
-
-export default appRoutes;
