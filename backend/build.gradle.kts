3db7f23199d821bbbed6ea946bf4e7a48b2b20e6
Fabian Winter <fabian.winter@tngtech.com>
2020-06-13 16:23:11 +0200

    Update lots of versions
    Only for Gradle, though.
    
    Signed-off-by: Fabian Winter <fabian.winter@tngtech.com>

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    id("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
-    id("org.springframework.boot") version "2.2.3.RELEASE"
+    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.3.72"
 
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
+
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("se.patrikerdes.use-latest-versions") version "0.2.14"
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 repositories {
@@ -20,10 +23,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    testImplementation("com.tngtech.archunit:archunit-junit5-api:0.9.3")
-    testImplementation("com.tngtech.archunit:archunit-junit5-engine:0.9.3")
+    testImplementation("com.tngtech.archunit:archunit-junit5-api:0.14.1")
+    testImplementation("com.tngtech.archunit:archunit-junit5-engine:0.14.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
