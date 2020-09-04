0e1afa7e163136afb3332d73ec67ac68df8f47e2
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-09-04 14:53:00 +0200

    add component to upload new versions
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

---
+++ frontend/src/app/version/version-create/version-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-version-create',
+  templateUrl: './version-create.component.html',
+  styleUrls: ['./version-create.component.scss']
+})
+export class VersionCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
