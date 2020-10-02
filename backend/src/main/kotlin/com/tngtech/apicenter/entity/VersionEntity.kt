1ec9ed27b57776253a85696188b83e56c77d760f
Fabian Winter <fabian.winter@tngtech.com>
2020-10-02 08:34:20 +0200

    Update backend dependencies
    Signed-off-by: Fabian Winter <fabian.winter@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/entity/VersionEntity.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/entity/VersionEntity.kt
@@ -10,13 +10,13 @@ import javax.validation.constraints.NotBlank
 
 @Entity(name = "version")
 data class VersionEntity(
-        @Id @GeneratedValue val id: UUID,
-        @field:NotBlank val title: String,
-        @field:NotBlank val version: String,
-        val description: String?,
-        @field:NotBlank val content: String,
+    @Id @GeneratedValue val id: UUID,
+    @field:NotBlank val title: String,
+    @field:NotBlank val version: String,
+    val description: String?,
+    @field:NotBlank val content: String,
 
-        @ManyToOne
-    @JoinColumn(name = "interface_id")
-    val interfaceEntity: InterfaceEntity
+    @ManyToOne
+@JoinColumn(name = "interface_id")
+val interfaceEntity: InterfaceEntity
 )
