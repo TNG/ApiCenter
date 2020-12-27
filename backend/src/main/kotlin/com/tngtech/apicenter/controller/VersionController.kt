0ace0baba6fafcf7094936a05370d7b772b21b87
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2020-12-27 11:39:47 +0100

    add deletion endpoint for versions
    Signed-off-by: Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>

--- backend/src/main/kotlin/com/tngtech/apicenter/controller/VersionController.kt
+++ backend/src/main/kotlin/com/tngtech/apicenter/controller/VersionController.kt
@@ -3,12 +3,15 @@ package com.tngtech.apicenter.controller
 import com.tngtech.apicenter.dto.VersionFileDto
 import com.tngtech.apicenter.service.VersionService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/versions")
@@ -20,4 +23,8 @@ class VersionController(private val versionService: VersionService) {
 
     @GetMapping
     fun getVersions() = versionService.getVersions()
+
+    @DeleteMapping("/{versionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteVersions(@PathVariable versionId: UUID) = versionService.deleteVersion(versionId)
 }
